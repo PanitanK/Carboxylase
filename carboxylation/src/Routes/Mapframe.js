4a06744810d0dce917aacb62b7d5759040086c08
PanitanK <98577467+PanitanK@users.noreply.github.com>
2023-08-18 11:25:39 +0700

    18/8 Mittag

--- carboxylation/src/Routes/Mapframe.js
+++ carboxylation/src/Routes/Mapframe.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const MapFrame = () => {
+  const [polygonCoordinates, setPolygonCoordinates] = useState([]);
+
   useEffect(() => {
     window.addEventListener("message", receiveMessage);
 
@@ -11,9 +13,27 @@ const MapFrame = () => {
 
   const receiveMessage = (event) => {
     if (event.data.type === "polygonCoordinates") {
-      const polygonCoordinates = event.data.coordinates;
-      console.log("Received polygon coordinates:", polygonCoordinates);
+      const receivedCoordinates = event.data.coordinates;
+      setPolygonCoordinates(receivedCoordinates);
     }
+  };
+
+  const calculatePolygonArea = (coords) => {
+    if (coords.length < 3) return 0;
+
+    let area = 0;
+
+    for (let i = 0; i < coords.length; i++) {
+      const curr = coords[i];
+      const next = coords[(i + 1) % coords.length];
+
+      area += (next.lng + curr.lng) * (next.lat - curr.lat);
+    }
+
+    // Conversion factor: 1 sq degree = 111.32 sq km (approximately)
+    const sqKmArea = (Math.abs(area) / 2) * 111.32 * 111.32;
+
+    return sqKmArea;
   };
 
   return (
@@ -26,6 +46,17 @@ const MapFrame = () => {
         frameBorder="0"
         title="Google Map"
       ></iframe>
+      <div>
+        <h2>Polygon Coordinates:</h2>
+        <textarea
+          rows={5}
+          cols={40}
+          readOnly
+          value={JSON.stringify(polygonCoordinates)}
+        />
+        <h2>Polygon Area:</h2>
+        <p>{calculatePolygonArea(polygonCoordinates).toFixed(2)} sq km</p>
+      </div>
     </div>
   );
 };
