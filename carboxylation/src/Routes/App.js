f46a78fb646ddc660627ff50c6a19fb7e912b5a7
PanitanK <98577467+PanitanK@users.noreply.github.com>
2023-08-22 20:13:31 +0700

    Update

--- carboxylation/src/Routes/App.js
+++ carboxylation/src/Routes/App.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Title from './image/logo/CBX_Transparent.png';
 import './css/App.css';
 import Mapframe from "./Mapframe";
-
+import Credit from './image/logo/Credit.png'
 
 function App() {
 
@@ -29,12 +29,27 @@ function App() {
       </div>
 
       <header className="App-header">
-        <p >Main Page</p>
+       <h1 className="primary-header" style={{ marginTop:"150px" , color:'black',transform: "scale(1.2)"}}>
+        
+        Carboxylation <br></br><br></br>Your Carbon Credit Validation Solution</h1>
+
+      <img  style={{ height:'500px' ,marginTop:"50px"
+      
+    }}
+            src={Credit}
+            alt="Credit">
+            
+            </img>
+
+        <div style={{ transform: "scale(0.8)" }}  >
         <Mapframe />
+        </div>
+
         <Link to="/Register">
           <button class="btn2" style={{ fontSize :'20px' ,paddingTop: '20px', paddingBottom: '20px' , paddingLeft: '40px' , paddingRight: '40px' }}>Join Us</button>
         </Link>
         <div className="footer">
+          
           
         </div>
       </header>
